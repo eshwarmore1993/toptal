db7e0320eff767095bc61927b255f1c15c2c332d
eshwarmore1993 <eshwar.more@gmail.com>
2017-05-10 19:39:19 +0530

    tree

---
+++ Tree.java
@@ -0,0 +1,12 @@
+public class Tree {
+    public int x;
+    public Tree l;
+    public Tree r;
+
+    Tree(int x, Tree l, Tree r)
+    {
+        this.x= x;
+        this.l=l;
+        this.r=r;
+    }
+}
